ccd7cab515da6c03d8d4681866c03dfd7d72422b
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2022-04-21 18:51:36 +0530

    new version 1.0

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# importing all necessary packages
+
+
 
 from flask import Flask,flash
 from flask import Blueprint, render_template, url_for,request,redirect
@@ -11,14 +14,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer,ListTrainer
 import os
 
-
-from googletrans import Translator
-translator = Translator()
-
-
-# import demoji
-# demoji.download_codes()
-
+#training the chatterbot algo
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databasesqlite.sqlite3'
@@ -56,28 +52,43 @@ english_bot.set_trainer(ListTrainer)
 # english_bot.set_trainer(ChatterBotCorpusTrainer)
 # english_bot.train("chatterbot.corpus.english")
 
+
+#different ways of flow of html pages using render template  
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
 
-@app.route('/login',methods=["POST"])
-def login_post():
-    name=request.form['name']
-    password=request.form['pwd']
 
-    cursor=databasesqlite.query.filter_by(name=name,pwd=password).all()
-    cursor12=databasesqlite.query.filter_by(name=name).first()
-    
-    if cursor:
-        return render_template('sampleNLP.html',name=name)
-    elif (cursor12):
-        flash("Incorrect Password")
-    else:
-        flash("User name doesn't exists")
-    return render_template('index.html')
+@app.route('/account')
+def account():
+    return render_template('account.html')
 
-@app.route('/update',methods=["POST"])
-def login_get():
+@app.route('/new-account',methods=["POST"])
+def account1():
+        account = databasesqlite(request.form['name'], request.form['pwd'],request.form['email'],request.form['no'])
+        user= databasesqlite.query.filter_by(name=request.form['name']).first()
+        if user:
+            flash("User name already exists")
+            return render_template('account.html')
+
+        else:
+            # flash("Account created successfully")
+            db.session.add(account)
+            db.session.commit()
+            flash("Account created Successfully")
+            return render_template('login.html')
+ 
+
+@app.route('/forget_password')
+def forgot():
+    return render_template('forget_password.html')
+
+@app.route('/update-password',methods=["POST"])
+def update():
     name=request.form['name']
     password=request.form['pwd']
 
@@ -89,64 +100,46 @@ def login_get():
         rows_updated = databasesqlite.query.filter_by(name=name).update(dict(pwd=password))
         db.session.commit()
         flash("Password changed Successfully")
-        return render_template('index.html')
-    return render_template('forgetpassword.html')
+        return render_template('login.html')
+    return render_template('forget_password.html')
 
-@app.route('/signup',methods=["POST"])
-def signup_post():
 
-        account = databasesqlite(request.form['name'], request.form['pwd'],request.form['email'],request.form['no'])
-        user= databasesqlite.query.filter_by(name=request.form['name']).first()
-        if user:
-            flash("User already exists")
-            return render_template('create-account.html')
-
-        else:
-            # flash("Account created successfully")
-            db.session.add(account)
-            db.session.commit()
-            flash("Account created Successfully")
-            return render_template('index.html')
-        
-@app.route('/chatbot')
+@app.route('/chatbot',methods=["POST"])
 def chatbot():
+    name=request.form['name']
+    password=request.form['pwd']
 
-    return render_template('sampleNLP.html')
+    cursor=databasesqlite.query.filter_by(name=name,pwd=password).all()
+    cursor12=databasesqlite.query.filter_by(name=name).first()
+    
+    if cursor:
+        return render_template('chatbot(o).html',name=name)
+    elif (cursor12):
+        flash("Incorrect Password")
+    else:
+        flash("User name doesn't exists")
+    
+    return render_template('login.html')
 
 
-@app.route('/signup')
-def signup():
-    return render_template('create-account.html')
 
-@app.route('/forget')
-def forgetpage():
-    return render_template('forgetpassword.html')
-
-@app.route('/logout')
-def logout():
-    return render_template('index.html')
-
-@app.route("/get")
+@app.route('/get')
 def get_bot_response():
+    # userText = request.form['textInput']
     userText = request.args.get('msg')
     print(userText) 
     # result=translator.translate(str(userText), dest='en')
-    # userText=result.text
-    # print(userText)
+    # userText1=result.text
+    # print(userText1)
     response = str(english_bot.get_response(userText))
 
 
     # response=translator.translate(response, dest=result.src)
-
-    # s=response.split()
-    # n=len(s)
-    # if(n<10):
-    #     b=n
-    # else:
-    #     a=(n/10)*10
-    #     b=a+10
-        # print(b)    
+    print(response)
     return response
+    # flash (response)
+
+
 
 if __name__ == "__main__":
     db.create_all()
